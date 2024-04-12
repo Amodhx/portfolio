d31cc4cd09120e6e1f7e264c000044195680767d
Amodhx <amxdhnanditha@gmail.com>
2024-04-12 10:55:30 +0530

    burger button done

--- js/Main.js
+++ js/Main.js
@@ -1,25 +1,40 @@
-let nodeListOf = document.querySelectorAll("section");
-
-document.addEventListener('scroll', function (){
-    const  current = window.pageYOffset;
-    nodeListOf.forEach((section, index) => {
-        const sectionTop = section.offsetTop;
-
-        if (current == sectionTop){
-            // console.log(sectionTop+"adsd")
-        }else {
-            // console.log(sectionTop);
-        }
-    });
+const observer = new IntersectionObserver((entries) =>{
+   entries.forEach((entry) =>{
+      if (entry.isIntersecting){
+        entry.target.classList.add('show');
+      }else {
+        entry.target.classList.remove('show');
+      }
+   });
 });
 
-document.addEventListener('scrollend' , function (){
-    for (let i = 0; i < nodeListOf.length; i++) {
-        if (nodeListOf[i].pageYOffset == window.pageYOffset){
-            console.log("A")
-        }
+
+let elementNodeListOf = document.querySelectorAll(".hidden");
+elementNodeListOf.forEach((el) =>observer.observe(el));
+var x = 0;
+
+document.querySelector("#burgetbtn").addEventListener('click',function (){
+    if (x == 0) {
+        x = 1;
+        let element = document.querySelector("#burgetbtn>div:nth-child(2)");
+        element.classList.add('hide');
+        let element1 = document.querySelector('#burgetbtn>div:nth-child(3)');
+        element1.classList.add('rotate3');
+        let element2 = document.querySelector('#burgetbtn>div:first-child');
+        element2.classList.add('rotate1');
+
+        let htmlElement = document.querySelector('header');
+        htmlElement.classList.add('show');
+    }else {
+        let element = document.querySelector("#burgetbtn>div:nth-child(2)");
+        element.classList.remove('hide');
+        let element1 = document.querySelector('#burgetbtn>div:nth-child(3)');
+        element1.classList.remove('rotate3');
+        let element2 = document.querySelector('#burgetbtn>div:first-child');
+        element2.classList.remove('rotate1');
+        x = 0;
+
+        let htmlElement = document.querySelector('header');
+        htmlElement.classList.remove('show');
     }
 })
-
-
-
